4d7763c19e80655fe6e2edaefc7341377babb20c
karthikreddybilakanti-ops <karthikreddybilakanti@gmail.com>
2026-05-30 12:38:24 +0530

    fixed start command

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import os
 from dotenv import load_dotenv
@@ -57,3 +58,5 @@ def generate():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
